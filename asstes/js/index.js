6828d4a4de2fd5a25f0806366d7342515a0f2ff3
khaled El-henaweey <71648492+khaled-elhenaweey@users.noreply.github.com>
2024-05-28 14:22:42 +0300

    set the none note flow

--- asstes/js/index.js
+++ asstes/js/index.js
@@ -1,78 +1,69 @@
-let tasks = [
-  {
-    name: "first note ",
-    date: "27/5/2024",
-    state: false,
-  },
-  {
-    name: "second note",
-    date: "28/5/2024",
-    state: true,
-  },
-  {
-    name: "third note",
-    date: "29/5/2024",
-    state: false,
-  },
-  {
-    name: "final note",
-    date: "30/5/2024",
-    state: true,
-  },
-];
+let tasks = [];
 getLocal();
 function callTasksList() {
   document.querySelector(".table-body").innerHTML = "";
-  let index = 0;
-  for (const task of tasks) {
+  if (tasks.length == 0) {
     let content = `
-      <tr class="${task.state ? "done" : ""}">
-      <td>
-          <div class="note-data">
-              <h4>${task.name}</h4>
-              <div class="note-date">
-                  <span class="material-symbols-outlined">
-                      event_note
-                  </span>
-                  <span>${task.date}</span>
-              </div>
-          </div>
-      </td>
-      <td>
-          <div class="note-action">
-              <button class="btn btn-icon btn-icon-primary" onclick="editTask(${index})">
-                  <span class="material-symbols-outlined">
-                      edit_square
-                  </span>
-              </button>
-
-              <button class="btn btn-icon btn-icon-danger" onclick="deleteTask(${index})">
-                  <span class="material-symbols-outlined">
-                      delete
-                  </span>
-              </button>
-
-              <button class="btn btn-icon btn-icon-danger ${
-                task.state ? "flex" : "none"
-              }" onclick="checkTask(${index})">
-                  <span class="material-symbols-outlined">
-                      disabled_by_default
-                  </span>
-              </button>
-
-              <button class="btn btn-icon btn-icon-success ${
-                task.state ? "none" : "flex"
-              }"   onclick="checkTask(${index})">
-                  <span class="material-symbols-outlined">
-                      select_check_box
-                  </span>
-              </button>
-          </div>
-      </td>
+    <tr class="no-notes">
+    <td colspan ="2">
+    Hi please use the add button to add your first note 
+    </td>
     </tr>
-        `;
+    `;
     document.querySelector(".table-body").innerHTML += content;
-    index++;
+  } else {
+    let index = 0;
+    for (const task of tasks) {
+      let content = `
+        
+          <tr class="${task.state ? "done" : ""}">
+          <td>
+              <div class="note-data">
+                  <h4>${task.name}</h4>
+                  <div class="note-date">
+                      <span class="material-symbols-outlined">
+                          event_note
+                      </span>
+                      <span>${task.date}</span>
+                  </div>
+              </div>
+          </td>
+          <td>
+              <div class="note-action">
+                  <button class="btn btn-icon btn-icon-primary" onclick="editTask(${index})">
+                      <span class="material-symbols-outlined">
+                          edit_square
+                      </span>
+                  </button>
+    
+                  <button class="btn btn-icon btn-icon-danger" onclick="deleteTask(${index})">
+                      <span class="material-symbols-outlined">
+                          delete
+                      </span>
+                  </button>
+    
+                  <button class="btn btn-icon btn-icon-danger ${
+                    task.state ? "flex" : "none"
+                  }" onclick="checkTask(${index})">
+                      <span class="material-symbols-outlined">
+                          disabled_by_default
+                      </span>
+                  </button>
+    
+                  <button class="btn btn-icon btn-icon-success ${
+                    task.state ? "none" : "flex"
+                  }"   onclick="checkTask(${index})">
+                      <span class="material-symbols-outlined">
+                          select_check_box
+                      </span>
+                  </button>
+              </div>
+          </td>
+        </tr>
+            `;
+      document.querySelector(".table-body").innerHTML += content;
+      index++;
+    }
   }
 }
 
